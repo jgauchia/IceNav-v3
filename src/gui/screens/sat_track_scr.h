b8a7c81c07b124741194a8be5e1befd64157a3d0
jgauchia <jgauchia@gmx.es>
2022-10-12 15:05:39 +0200

    Changed Sat Track screen to Dark Theme

--- src/gui/screens/sat_track_scr.h
+++ src/gui/screens/sat_track_scr.h
@@ -8,19 +8,24 @@
 
 /**
  * @brief Get the sat tracking info (elevation, azimunt, snr, active,...)
- * 
+ *
  */
 void get_sat_tracking()
 {
   char s_buf[64];
   Latitude_formatString(5, 5, 2, GPS.location.lat());
   Longitude_formatString(5, 20, 2, GPS.location.lng());
+  tft.setTextPadding(tft.textWidth("88", 2));
   tft.drawNumber(GPS.satellites.value(), 35, 50);
-
+  tft.setTextPadding(0);
+  tft.setTextPadding(tft.textWidth("8888 m", 4));
   sprintf(s_buf, "%4d m", (int)GPS.altitude.meters());
   tft.drawString(s_buf, 0, 130, 4);
+  tft.setTextPadding(0);
+  tft.setTextPadding(tft.textWidth("888.8", 4));
   sprintf(s_buf, "%2.1f", (double)GPS.hdop.hdop());
   tft.drawString(s_buf, 5, 90, 4);
+  tft.setTextPadding(0);
 
   if (totalGPGSVMessages.isUpdated())
   {
@@ -44,8 +49,6 @@ void get_sat_tracking()
       {
         if (sat_tracker[i].pos_x != 0 && sat_tracker[i].pos_y != 0)
         {
-          sat_sprite.fillCircle(4, 4, 4, TFT_WHITE);
-          sat_sprite.pushSprite(sat_tracker[i].pos_x, sat_tracker[i].pos_y);
           tft.startWrite();
           tft.setCursor(sat_tracker[i].pos_x, sat_tracker[i].pos_y + 5, 1);
           tft.print("  ");
@@ -53,9 +56,9 @@ void get_sat_tracking()
         }
       }
       tft.startWrite();
-      tft.drawCircle(165, 80, 60, TFT_BLACK);
-      tft.drawCircle(165, 80, 30, TFT_BLACK);
-      tft.drawCircle(165, 80, 1, TFT_BLACK);
+      tft.drawCircle(165, 80, 60, TFT_WHITE);
+      tft.drawCircle(165, 80, 30, TFT_WHITE);
+      tft.drawCircle(165, 80, 1, TFT_WHITE);
       tft.drawString("N", 162, 12, 2);
       tft.drawString("S", 162, 132, 2);
       tft.drawString("O", 102, 72, 2);
@@ -74,12 +77,12 @@ void get_sat_tracking()
           if (active_sat < 12)
           {
             tft.setCursor((active_sat * 20) + 8, 229, 1);
-            tft.fillRect((active_sat * 20) + 5, 224 - (sat_tracker[i].snr), 15, (sat_tracker[i].snr), TFT_DARKCYAN);
+            tft.fillRect((active_sat * 20) + 5, 224 - (sat_tracker[i].snr), 15, (sat_tracker[i].snr), TFT_RED);
           }
           else
           {
             tft.setCursor(((active_sat - 12) * 20) + 8, 310, 1);
-            tft.fillRect(((active_sat - 12) * 20) + 5, 305 - (sat_tracker[i].snr), 15, (sat_tracker[i].snr), TFT_DARKCYAN);
+            tft.fillRect(((active_sat - 12) * 20) + 5, 305 - (sat_tracker[i].snr), 15, (sat_tracker[i].snr), TFT_RED);
           }
           tft.print(i + 1);
           active_sat++;
@@ -88,8 +91,8 @@ void get_sat_tracking()
           int sat_pos_y = 80 - (H * cos(DEGtoRAD(sat_tracker[i].azimuth)));
           sat_tracker[i].pos_x = sat_pos_x;
           sat_tracker[i].pos_y = sat_pos_y;
-          sat_sprite.fillCircle(4, 4, 4, TFT_GREEN);
-          sat_sprite.pushSprite(sat_pos_x, sat_pos_y);
+          sat_sprite.fillCircle(2, 2, 2, TFT_GREEN);
+          sat_sprite.pushSprite(sat_pos_x, sat_pos_y, TFT_TRANSPARENT);
           tft.setCursor(sat_pos_x, sat_pos_y + 5, 1);
           tft.print(i + 1);
         }
@@ -100,7 +103,7 @@ void get_sat_tracking()
 
 /**
  * @brief Display satellite track screen, GPS Position, number satellites, location
- * 
+ *
  */
 void show_sat_track_screen()
 {
@@ -109,20 +112,21 @@ void show_sat_track_screen()
     tft.startWrite();
     sat_sprite.deleteSprite();
     sat_sprite.createSprite(8, 8);
-    sat_sprite.fillScreen(TFT_WHITE);
-    tft.fillScreen(TFT_WHITE);
-    tft.setTextColor(TFT_BLACK, TFT_WHITE);
-    tft.fillRect(0, 159, 240, 2, TFT_BLACK);
-    tft.fillRect(0, 240, 240, 2, TFT_BLACK);
+    sat_sprite.setColorDepth(8);
+    sat_sprite.fillSprite(TFT_TRANSPARENT);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.fillRect(0, 159, 240, 2, TFT_WHITE);
+    tft.fillRect(0, 240, 240, 2, TFT_WHITE);
     for (int i = 0; i < 7; i++)
     {
       tft.drawLine(0, 224 - (i * 10), 240, 224 - (i * 10), TFT_LIGHTGREY);
       tft.drawLine(0, 305 - (i * 10), 240, 305 - (i * 10), TFT_LIGHTGREY);
     }
     tft.readRect(0, 159, 25, 80, snr_bkg);
-    tft.drawCircle(165, 80, 60, TFT_BLACK);
-    tft.drawCircle(165, 80, 30, TFT_BLACK);
-    tft.drawCircle(165, 80, 1, TFT_BLACK);
+    tft.drawCircle(165, 80, 60, TFT_WHITE);
+    tft.drawCircle(165, 80, 30, TFT_WHITE);
+    tft.drawCircle(165, 80, 1, TFT_WHITE);
     tft.drawString("N", 162, 12, 2);
     tft.drawString("S", 162, 132, 2);
     tft.drawString("O", 102, 72, 2);
