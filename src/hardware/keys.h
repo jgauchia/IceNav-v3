ccefb22cd31c85a2153450de68e690d950803465
jgauchia <jgauchia@gmx.es>
2022-10-16 14:48:54 +0200

    Added Keys Debounce

--- src/hardware/keys.h
+++ src/hardware/keys.h
@@ -12,6 +12,10 @@ PCF8574 keyboard(0x20);
 #endif
 
 #include "hardware/keys_def.h"
+int buttonState;
+int lastButtonState = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 100;
 
 /**
  * @brief Read keys
@@ -22,36 +26,49 @@ static int Read_Keys()
 {
 #ifdef ENABLE_PCF8574
   keyboard.read8();
-  switch (keyboard.value())
+  int reading = keyboard.value();
+  if (reading != lastButtonState)
   {
-  case 223:
-    return LUP;
-    break;
-  case 191:
-    return LBUT;
-    break;
-  case 127:
-    return LDOWN;
-    break;
-  case 239:
-    return UP;
-    break;
-  case 254:
-    return DOWN;
-    break;
-  case 251:
-    return LEFT;
-    break;
-  case 253:
-    return RIGHT;
-    break;
-  case 247:
-    return PUSH;
-    break;
-  default:
-    return NONE;
-    break;
+    lastDebounceTime = millis();
   }
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+      switch (buttonState)
+      {
+      case 223:
+        return LUP;
+        break;
+      case 191:
+        return LBUT;
+        break;
+      case 127:
+        return LDOWN;
+        break;
+      case 239:
+        return UP;
+        break;
+      case 254:
+        return DOWN;
+        break;
+      case 251:
+        return LEFT;
+        break;
+      case 253:
+        return RIGHT;
+        break;
+      case 247:
+        return PUSH;
+        break;
+      default:
+        return NONE;
+        break;
+      }
+    }
+  }
+  lastButtonState = reading;
+  return NONE;
 #endif
 }
-
