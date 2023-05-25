15426f2f8c443fabe45fbb008a1aeca51198ebd2
jgauchia <jgauchia@gmx.es>
2023-05-25 11:54:50 +0200

    Fix Output

--- src/gui/lvgl.h
+++ src/gui/lvgl.h
@@ -81,7 +81,9 @@ void touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
         data->state = LV_INDEV_STATE_PRESSED;
         data->point.x = touchX;
         data->point.y = touchY;
-        debug->println(touchX);
+        debug->print("X: ");
+        debug->print(touchX);
+        debug->print("  Y: ");
         debug->println(touchY);
     }
 }
