371b1ad6dfddea0401f0c91e85853ea0ec6cbd1e
jgauchia <jgauchia@gmx.es>
2023-06-14 23:43:24 +0200

    Firs test of smoothing (James Bruton filter)

--- src/hardware/compass.h
+++ src/hardware/compass.h
@@ -16,26 +16,28 @@ Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified(12345);
 #ifdef MAKERF_ESP32S3
 #include <MPU9250.h>
 
-MPU9250 IMU(Wire,0x68);
+MPU9250 IMU(Wire, 0x68);
 #endif
 
 /**
  * @brief Magnetic declination
- * 
+ *
  */
 //  Obtain your magnetic declination from http://www.magnetic-declination.com/
 //  By convention, declination is positive when magnetic north
 //  is east of true north, and negative when it is to the west.
 //  Substitute your magnetic declination for the "declinationAngle" shown below.
-float declinationAngle = 0.22;    
-
+float declinationAngle = 0.22;
 
 /**
  * @brief Compass Heading Angle
- * 
+ *
  */
-uint16_t heading = 0;
-
+int heading = 0;
+float heading_smooth = 0.0;
+float heading_previous = 0.0;
+#define SMOOTH_FACTOR 0.35
+#define SMOOTH_PREVIOUS_FACTOR 0.65
 
 /**
  * @brief Read compass data
@@ -44,47 +46,49 @@ uint16_t heading = 0;
  */
 int read_compass()
 {
-    float y = 0.0;
-    float x = 0.0;
-    float z = 0.0;
+  float y = 0.0;
+  float x = 0.0;
+  float z = 0.0;
 
 #ifdef CUSTOMBOARD
-    sensors_event_t event;
-    compass.getEvent(&event);
-    y = event.magnetic.y;
-    x = event.magnetic.x;
-    z = event.magnetic.z;
+  sensors_event_t event;
+  compass.getEvent(&event);
+  y = event.magnetic.y;
+  x = event.magnetic.x;
+  z = event.magnetic.z;
 #endif
 
 #ifdef MAKERF_ESP32S3
-    IMU.readSensor();
-    x = IMU.getMagX_uT();
-    y = IMU.getMagY_uT();
-    z = IMU.getMagZ_uT();
+  IMU.readSensor();
+  x = IMU.getMagX_uT();
+  y = IMU.getMagY_uT();
+  z = IMU.getMagZ_uT();
 #endif
-    float heading = atan2(y,x);
+  float heading_no_filter = atan2(y, x);
+  heading_no_filter += declinationAngle;
+  heading_smooth = (heading_no_filter * SMOOTH_FACTOR) + (heading_previous * SMOOTH_PREVIOUS_FACTOR);
+  heading_previous = heading_smooth;
 
-    heading += declinationAngle;
-    if (heading < 0)
-        heading += 2 * PI;
-    if (heading > 2 * PI)
-        heading -= 2 * PI;
-    return (uint16_t)(heading * 180 / M_PI);
-
+  if (heading_smooth < 0)
+    heading_smooth += 2 * M_PI;
+  if (heading_smooth > 2 * M_PI)
+    heading_smooth -= 2 * M_PI;
+  return (int)(heading_smooth * 180 / M_PI);
 }
 
-void init_compass(){
+void init_compass()
+{
 
 #ifdef CUSTOMBOARD
   compass.begin();
 #endif
 #ifdef MAKERF_ESP32S3
   int status = IMU.begin();
-  if (status < 0) {
+  if (status < 0)
+  {
     log_e("IMU initialization unsuccessful");
     log_e("Check IMU wiring or try cycling power");
-    log_e("Status: %i",status);
+    log_e("Status: %i", status);
   }
 #endif
 }
-
