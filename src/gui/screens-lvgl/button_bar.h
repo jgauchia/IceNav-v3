504988179b9f44ef8b95554aecf7a75207420f3e
jgauchia <jgauchia@gmx.es>
2023-06-24 21:19:46 +0200

    Change Button Bar Background color

--- src/gui/screens-lvgl/button_bar.h
+++ src/gui/screens-lvgl/button_bar.h
@@ -20,15 +20,19 @@ void create_button_bar_scr()
     lv_obj_set_size(buttonBar, TFT_WIDTH, 68);
     lv_obj_set_pos(buttonBar, 0, TFT_HEIGHT - 68);
     lv_obj_set_flex_flow(buttonBar, LV_FLEX_FLOW_ROW);
-    lv_obj_set_flex_align(buttonBar, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER,LV_FLEX_ALIGN_CENTER);
+    lv_obj_set_flex_align(buttonBar, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
     lv_obj_clear_flag(buttonBar, LV_OBJ_FLAG_SCROLLABLE);
+    static lv_style_t style_bar;
+    lv_style_init(&style_bar);
+    lv_style_set_bg_opa(&style_bar, LV_OPA_0);
+    lv_style_set_border_color(&style_bar, lv_color_black());
+    lv_obj_add_style(buttonBar, &style_bar, LV_PART_MAIN);
 
     // Settings Button
     lv_obj_t *settingsBtn = lv_img_create(buttonBar);
     lv_img_set_src(settingsBtn, "F:/settings.bin");
-    lv_obj_add_flag(settingsBtn,LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_event_cb(settingsBtn,test_cb,LV_EVENT_PRESSED,NULL);
-    
+    lv_obj_add_flag(settingsBtn, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_add_event_cb(settingsBtn, test_cb, LV_EVENT_PRESSED, NULL);
 }
 
 void test_cb(lv_event_t *event)
