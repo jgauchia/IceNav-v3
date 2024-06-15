15dc5d1e5607e1b293a6056edca8cd1498543807
Hpsaturn <hpsaturn@gmail.com>
2024-06-15 12:37:23 +0200

    screenshot feature, initial version to SD

--- lib/cli/cli.cpp
+++ lib/cli/cli.cpp
@@ -8,6 +8,8 @@
 
 #ifndef DISABLE_CLI
 #include "cli.hpp"
+#include "storage.hpp"
+#include "tft.hpp"
 
 const char logo[] =
 "\r\n"
@@ -22,6 +24,40 @@ const char logo[] =
 ""
 ;
 
+// Capture the screenshot and save it to the SD card
+void captureScreenshot(const char* filename, Stream *response) {
+  File file = SD.open(filename, FILE_WRITE);
+  if (!file) {
+    response->println("Failed to open file for writing");
+    return;
+  }
+
+  // Allocate memory to store the screen data
+  response->printf("Allocating memory for: %ix%i image\r\n",tft.width(),tft.height());
+  uint8_t* buffer = (uint8_t*)ps_malloc(tft.width() * tft.height() * sizeof(uint8_t));
+  if (!buffer) {
+    response->println("Failed to allocate memory for buffer");
+    file.close();
+    return;
+  }
+
+  // Read the screen data into the buffer
+  tft.readRect(0, 0, tft.width(), tft.height(), buffer);
+
+  // Write the buffer data to the file
+  for (int y = 0; y < tft.height(); y++) {
+    for (int x = 0; x < tft.width(); x++) {
+      uint16_t color = buffer[y * tft.width() + x];
+      file.write((const uint8_t*)&color, sizeof(color));
+    }
+  }
+
+  // Clean up
+  free(buffer);
+  file.close();
+  response->println("Screenshot saved");
+}
+
 void wcli_reboot(char *args, Stream *response) {
   ESP.restart();
 } 
@@ -33,6 +69,10 @@ void wcli_info(char *args, Stream *response) {
 
 void wcli_clear(char *args, Stream *response){
   wcli.shell->clear();
+}
+
+void wcli_scshot(char *args, Stream *response){
+  captureScreenshot("/screenshot.raw",response);
 }
 
 /**
@@ -48,7 +88,7 @@ void initCLI() {
   wcli.add("reboot", &wcli_reboot, "\tperform a ESP32 reboot");
   wcli.add("info", &wcli_info, "\t\tget device information");
   wcli.add("clear", &wcli_clear, "\t\tclear shell");
-  wcli.add("scshot", &wcli_clear, "\ttake screen shot");
+  wcli.add("scshot", &wcli_scshot, "\ttake screen shot");
   
   wcli.begin("IceNav");
 
