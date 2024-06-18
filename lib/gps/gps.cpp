5109a7451c9485bc69ad3b3821518d16abc2f07c
jgauchia <jgauchia@gmx.es>
2024-06-18 23:22:59 +0200

    Fix GPS init

--- lib/gps/gps.cpp
+++ lib/gps/gps.cpp
@@ -46,31 +46,34 @@ void initGPS()
 {
   gpsBaudDetected = autoBaudGPS();
 
+  if (gpsBaudDetected != 0)
+  {
   //gps->begin(GPS_BAUD[gpsBaud], SERIAL_8N1, GPS_RX, GPS_TX);
-  gps->begin(gpsBaudDetected, SERIAL_8N1, GPS_RX, GPS_TX);
+    gps->begin(gpsBaudDetected, SERIAL_8N1, GPS_RX, GPS_TX);
 
 
 #ifdef AT6558D_GPS
-  // GPS
-  // gps->println("$PCAS04,1*18\r\n");
+    // GPS
+    // gps->println("$PCAS04,1*18\r\n");
 
-  // GPS+GLONASS
-  // gps->println("$PCAS04,5*1C\r\n");
+    // GPS+GLONASS
+    // gps->println("$PCAS04,5*1C\r\n");
 
-  // GPS+BDS+GLONASS
-  gps->println("$PCAS04,7*1E\r\n");
-  gps->flush();
-  delay(100);
+    // GPS+BDS+GLONASS
+    gps->println("$PCAS04,7*1E\r\n");
+    gps->flush();
+    delay(100);
 
-  gps->println(GPS_RATE_PCAS[gpsUpdate]);
-  gps->flush();
-  delay(100);
+    gps->println(GPS_RATE_PCAS[gpsUpdate]);
+    gps->flush();
+    delay(100);
 
-  // Set NMEA 4.1
-  gps->println("$PCAS05,2*1A\r\n");
-  gps->flush();
-  delay(100);
+    // Set NMEA 4.1
+    gps->println("$PCAS05,2*1A\r\n");
+    gps->flush();
+    delay(100);
 #endif
+  }
 
   // Initialize satellites in view custom NMEA structure
   GPS_GSV.totalMsg.begin(GPS, PSTR("GPGSV"), 1);
