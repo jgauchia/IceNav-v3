888695805902d96a474ef9c64d4b66872430aeaa
Hpsaturn <hpsaturn@gmail.com>
2024-06-18 18:48:34 +0200

    added screenshot capture via wifi and some minor refactors

--- lib/cli/cli.cpp
+++ lib/cli/cli.cpp
@@ -8,8 +8,7 @@
 
 #ifndef DISABLE_CLI
 #include "cli.hpp"
-#include "storage.hpp"
-#include "tft.hpp"
+#include "utils.h"
 
 const char logo[] =
 "\r\n"
@@ -24,41 +23,7 @@ const char logo[] =
 ""
 ;
 
-// Capture the screenshot and save it to the SD card
-void captureScreenshot(const char* filename, Stream *response) {
-  File file = SD.open(filename, FILE_WRITE);
-  if (!file) {
-    response->println("Failed to open file for writing");
-    return;
-  }
 
-  // Allocate memory to store the screen data (1 byte per segment)
-  uint8_t* buffer = (uint8_t*)malloc(tft.width() * tft.height() * 2); // 2 bytes per pixel
-  if (!buffer) {
-    response->println("Failed to allocate memory for buffer");
-    file.close();
-    return;
-  }
-
-  // Read the screen data into the buffer using readRect
-  tft.readRect(0, 0, tft.width(), tft.height(), (uint16_t*)buffer);
-
-  // Write the buffer data to the file
-  for (int y = 0; y < tft.height(); y++) {
-    for (int x = 0; x < tft.width(); x++) {
-      // Combine the two 8-bit segments into a 16-bit value
-      uint8_t highByte = buffer[(y * tft.width() + x) * 2];
-      uint8_t lowByte = buffer[(y * tft.width() + x) * 2 + 1];
-      uint16_t color = (highByte << 8) | lowByte;
-      file.write((const uint8_t*)&color, sizeof(color));
-    }
-  }
-
-  // Clean up
-  free(buffer);
-  file.close();
-  response->println("Screenshot saved");
-}
 
 void wcli_reboot(char *args, Stream *response) {
   ESP.restart();
@@ -74,7 +39,20 @@ void wcli_clear(char *args, Stream *response){
 }
 
 void wcli_scshot(char *args, Stream *response){
-  captureScreenshot("/screenshot.raw",response);
+  Pair<String, String> operands = wcli.parseCommand(args);
+  String ip = operands.first();
+  uint16_t port = operands.second().toInt();
+ 
+  if (ip.isEmpty()){
+    response->println("Saving to SD..");
+    captureScreenshot(SCREENSHOT_TEMP_FILE, response);
+    response->println("Note: is possible to send it a PC using: scshot ip port");
+  }
+  else {
+    response->printf("Sending screenshot to %s:%i..\r\n", ip.c_str(), port);
+    captureScreenshot(SCREENSHOT_TEMP_FILE, response);
+    captureScreenshot(SCREENSHOT_TEMP_FILE, ip.c_str(), port);
+  }
 }
 
 void initRemoteShell(){
@@ -91,7 +69,7 @@ void initShell(){
   wcli.add("reboot", &wcli_reboot, "\tperform a ESP32 reboot");
   wcli.add("info", &wcli_info, "\t\tget device information");
   wcli.add("clear", &wcli_clear, "\t\tclear shell");
-  wcli.add("scshot", &wcli_scshot, "\ttake screen shot");
+  wcli.add("scshot", &wcli_scshot, "\tscreenshot to SD or sending a PC");
   wcli.begin("IceNav");
 }
 
