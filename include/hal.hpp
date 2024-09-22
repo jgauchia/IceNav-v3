b5e705fbdcb5eb7c256ec3b91724d5ff579f9871
Hpsaturn <hpsaturn@gmail.com>
2024-09-22 22:45:14 +0200

    copied the same hal entry to makerfabs only for try the first build

--- include/hal.hpp
+++ include/hal.hpp
@@ -205,6 +205,51 @@
   extern const uint8_t SD_CLK = 42;
 #endif
 
+#ifdef TDECK_ESP32S3
+  #define LCD_CS 37
+  #define LCD_BLK 45
+
+  #define I2C_SDA_PIN 38
+  #define I2C_SCL_PIN 39
+
+  extern const uint8_t GPS_TX = 17;
+  extern const uint8_t GPS_RX = 18;
+
+  extern const uint8_t SD_CS = 1;
+  extern const uint8_t SD_MISO = 41;
+  extern const uint8_t SD_MOSI = 2;
+  extern const uint8_t SD_CLK = 42;
+
+  extern const uint8_t TFT_WR  = 35;
+  extern const uint8_t TFT_RD  = 48;
+  extern const uint8_t TFT_RS  = 36;
+  extern const uint8_t TFT_RST = -1;
+  extern const uint8_t TFT_CS  = 5;
+  extern const uint8_t TFT_D0  = 47;
+  extern const uint8_t TFT_D1  = 21;
+  extern const uint8_t TFT_D2  = 14;
+  extern const uint8_t TFT_D3  = 13;
+  extern const uint8_t TFT_D4  = 12;
+  extern const uint8_t TFT_D5  = 11;
+  extern const uint8_t TFT_D6  = 10;
+  extern const uint8_t TFT_D7  = 9;
+  extern const uint8_t TFT_D8  = 3;
+  extern const uint8_t TFT_D9  = 8;
+  extern const uint8_t TFT_D10 = 16;
+  extern const uint8_t TFT_D11 = 15;
+  extern const uint8_t TFT_D12 = 7;
+  extern const uint8_t TFT_D13 = 6;
+  extern const uint8_t TFT_D14 = 5;
+  extern const uint8_t TFT_D15 = 4;
+
+  extern const uint8_t TCH_I2C_PORT = 0;  
+  extern const uint8_t TCH_I2C_SDA  = 38;
+  extern const uint8_t TCH_I2C_SCL  = 39;
+  extern const uint8_t TCH_I2C_INT  = 40;
+#endif
+
+
+
 /**
  * @brief TFT Invert color
  *
