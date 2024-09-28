0a99754ccce69458aab930d7e8c69a5322f67cf4
jgauchia <jgauchia@gmx.es>
2024-09-28 23:04:59 +0200

    style(nav): Clean code

--- lib/gui/src/mainScr.cpp
+++ lib/gui/src/mainScr.cpp
@@ -586,59 +586,7 @@ void createMainScr()
   
   // Navigation Tile
   navigationScr(navTile);
-  // lv_obj_t * label;
-  // label = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(label, fontOptions, 0);
-  // lv_label_set_text_static(label, "Navigation to:");
-  // lv_obj_center(label);
-  // lv_obj_align(label,LV_ALIGN_TOP_LEFT,10, 20);
 
-  // nameNav = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(nameNav, fontLargeMedium, 0);
-  // //lv_label_set_text_fmt(nameNav, "%s","");
-  // lv_obj_set_width(nameNav,TFT_WIDTH-10);
-  // lv_obj_set_pos(nameNav,10, 55);
-
-  // label = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(label, fontOptions, 0);
-  // lv_label_set_text_static(label, "Lat:");
-  // lv_obj_set_pos(label, 10, 90);
-
-  // label = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(label, fontOptions, 0);
-  // lv_label_set_text_static(label, "Lon:");
-  // lv_obj_set_pos(label, 10, 120);
-
-  // latNav = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(latNav, fontOptions, 0);
-  // lv_label_set_text_fmt(latNav, "%s", "");
-  // lv_obj_set_pos(latNav, 60, 90);
-  
-  // lonNav = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(lonNav, fontOptions, 0);
-  // lv_label_set_text_fmt(lonNav, "%s", "");
-  // lv_obj_set_pos(lonNav, 60, 120);
-
-  // label = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(label, fontOptions, 0);
-  // lv_label_set_text_static(label, "Distance");
-  // lv_obj_align(label, LV_ALIGN_CENTER, 0, -50);
-
-  // distNav = lv_label_create(navTile);
-  // lv_obj_set_style_text_font(distNav, fontVeryLarge, 0);
-  // lv_label_set_text_fmt(distNav,"%d m.", 0);
-  // lv_obj_align(distNav,LV_ALIGN_CENTER, 0, -5);
-
-  // arrowNav = lv_img_create(navTile);
-  // lv_img_set_zoom(arrowNav,iconScale);
-  // lv_obj_update_layout(arrowNav);
-  // lv_obj_align(arrowNav,LV_ALIGN_CENTER, 0, 100);
-  
-  // #ifdef ENABLE_COMPASS
-  //   lv_img_set_src(arrowNav, &navup);
-  //   lv_img_set_pivot(arrowNav, 50, 50) ;
-  // #endif
-  
   // Navigation Tile Events
   lv_obj_add_event_cb(navTile, updateNavEvent, LV_EVENT_VALUE_CHANGED, NULL);
   
