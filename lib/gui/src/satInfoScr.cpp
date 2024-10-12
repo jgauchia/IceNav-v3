d07ff5868fe79f09d8e3bb396f089caa42a20eaa
jgauchia <jgauchia@gmx.es>
2024-10-12 16:47:13 +0200

    feat(satinfo): Add Satellite info screen files

---
+++ lib/gui/src/satInfoScr.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file satInfoScr.hpp
+ * @author Jordi Gauchía (jgauchia@gmx.es)
+ * @brief  LVGL - Satellite info screen 
+ * @version 0.1.8_Alpha
+ * @date 2024-09
+ */
+
+ #include "satInfoScr.hpp"
---
+++ lib/gui/src/satInfoScr.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file satInfoScr.hpp
+ * @author Jordi Gauchía (jgauchia@gmx.es)
+ * @brief  LVGL - Satellite info screen 
+ * @version 0.1.8_Alpha
+ * @date 2024-09
+ */
+
+#ifndef SATINFOSCR_HPP
+#define SATINFOSCR_HPP
+
+#include "lvgl.h"
+#include "globalGuiDef.h"
+
+
+#endif
