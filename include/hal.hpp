087deebd82e48fc8b6f189b54e8c0f46b4490287
jgauchia <jgauchia@gmx.es>
2025-04-27 17:23:31 +0200

    refactor(tft): Refactor ICENAV panel definition

--- include/hal.hpp
+++ include/hal.hpp
@@ -14,8 +14,8 @@
  *
  */
 #ifdef ICENAV_BOARD
-  #define I2C_SDA_PIN 38
-  #define I2C_SCL_PIN 39
+  #define I2C_SDA_PIN   GPIO_NUM_38
+  #define I2C_SCL_PIN   GPIO_NUM_39
 
   uint8_t GPS_TX = 43;
   uint8_t GPS_RX = 44;
@@ -26,33 +26,42 @@
   extern const uint8_t SD_MISO = 41;
   extern const uint8_t SD_MOSI = 2;
   extern const uint8_t SD_CLK = 42;
+#endif
 
-  extern const uint8_t TFT_WR  = 18;
-  extern const uint8_t TFT_RD  = 48;
-  extern const uint8_t TFT_RS  = 45;
-  extern const uint8_t TFT_RST = -1;
-  extern const uint8_t TFT_CS  = -1;
-  extern const uint8_t TFT_D0  = 47;
-  extern const uint8_t TFT_D1  = 21;
-  extern const uint8_t TFT_D2  = 14;
-  extern const uint8_t TFT_D3  = 13;
-  extern const uint8_t TFT_D4  = 12;
-  extern const uint8_t TFT_D5  = 11;
-  extern const uint8_t TFT_D6  = 10;
-  extern const uint8_t TFT_D7  = 9;
-  extern const uint8_t TFT_D8  = 3;
-  extern const uint8_t TFT_D9  = 8;
-  extern const uint8_t TFT_D10 = 16;
-  extern const uint8_t TFT_D11 = 15;
-  extern const uint8_t TFT_D12 = 7;
-  extern const uint8_t TFT_D13 = 6;
-  extern const uint8_t TFT_D14 = 5;
-  extern const uint8_t TFT_D15 = 4;
+/**
+* @brief LilyGO T-DECK BOARD pin definition
+*
+*/
+#ifdef TDECK_ESP32S3   
+  #define I2C_SDA_PIN   GPIO_NUM_18
+  #define I2C_SCL_PIN   GPIO_NUM_8
 
-  extern const uint8_t TCH_I2C_PORT = 0;  
-  extern const uint8_t TCH_I2C_SDA  = 38;
-  extern const uint8_t TCH_I2C_SCL  = 39;
-  extern const uint8_t TCH_I2C_INT  = 40;
+  #define BOARD_POWERON 10
+  extern const uint8_t BOARD_BOOT_PIN = 0;
+
+  uint8_t GPS_TX = 43;
+  uint8_t GPS_RX = 44;
+
+  // extern const uint8_t TFT_SPI_SCLK = 40;
+  // extern const uint8_t TFT_SPI_MOSI = 41;
+  // extern const uint8_t TFT_SPI_MISO = 38;
+  // extern const uint8_t TFT_SPI_DC   = 11;
+  // extern const uint8_t TFT_SPI_CS   = 12;
+  // extern const uint8_t TFT_SPI_RST  = -1;
+  // extern const uint8_t TFT_SPI_BL   = 42;
+
+  extern const uint8_t SD_CS = GPIO_NUM_39;
+  extern const uint8_t SD_MISO = GPIO_NUM_38;
+  extern const uint8_t SD_MOSI = GPIO_NUM_41;
+  extern const uint8_t SD_CLK = GPIO_NUM_40;
+
+  // extern const uint8_t BOARD_TFT_CS = 12;
+  // extern const uint8_t RADIO_CS_PIN = 9;
+
+  // extern const uint8_t TCH_I2C_PORT = 0;
+  // extern const uint8_t TCH_I2C_SDA  = 18;
+  // extern const uint8_t TCH_I2C_SCL  = 8;
+  // extern const uint8_t TCH_I2C_INT  = 16;
 #endif
 
 /**
@@ -107,38 +116,6 @@
   extern const uint8_t TCH_I2C_SDA  = 38;
   extern const uint8_t TCH_I2C_SCL  = 39;
   extern const uint8_t TCH_I2C_INT  = -1;
-#endif
-
-#ifdef TDECK_ESP32S3   // Check GPS UART GPIO
-  #define I2C_SDA_PIN 18
-  #define I2C_SCL_PIN 8
-
-  #define BOARD_POWERON 10
-  extern const uint8_t BOARD_BOOT_PIN = 0;
-
-  uint8_t GPS_TX = 43;
-  uint8_t GPS_RX = 44;
-
-  // extern const uint8_t TFT_SPI_SCLK = 40;
-  // extern const uint8_t TFT_SPI_MOSI = 41;
-  // extern const uint8_t TFT_SPI_MISO = 38;
-  // extern const uint8_t TFT_SPI_DC   = 11;
-  // extern const uint8_t TFT_SPI_CS   = 12;
-  // extern const uint8_t TFT_SPI_RST  = -1;
-  // extern const uint8_t TFT_SPI_BL   = 42;
-
-  extern const uint8_t SD_CS = GPIO_NUM_39;
-  extern const uint8_t SD_MISO = GPIO_NUM_38;
-  extern const uint8_t SD_MOSI = GPIO_NUM_41;
-  extern const uint8_t SD_CLK = GPIO_NUM_40;
-
-  // extern const uint8_t BOARD_TFT_CS = 12;
-  // extern const uint8_t RADIO_CS_PIN = 9;
-
-  // extern const uint8_t TCH_I2C_PORT = 0;
-  // extern const uint8_t TCH_I2C_SDA  = 18;
-  // extern const uint8_t TCH_I2C_SCL  = 8;
-  // extern const uint8_t TCH_I2C_INT  = 16;
 #endif
 
 /**
--- lib/panel/LILYGO_TDECK.hpp
+++ lib/panel/LILYGO_TDECK.hpp
@@ -1,7 +1,7 @@
 /**
  * @file LILYGO_TDECK.hpp
  * @author Jordi Gauchía (jgauchia@jgauchia.com) and Antonio Vanegas @Hpsturn
- * @brief  LOVYANGFX TFT driver for ST7789 SPI With GT911 Touch controller
+ * @brief  LOVYANGFX TFT driver for Lilygo T-DECK biard
  * @version 0.2.1_alpha
  * @date 2025-04
  */
