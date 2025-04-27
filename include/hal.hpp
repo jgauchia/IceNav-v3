ffc82e2db0aaa79136191c4c1d975083789979b4
jgauchia <jgauchia@gmx.es>
2025-04-27 18:21:58 +0200

    refactor(tft): Refactor other panels pinout definition

--- include/hal.hpp
+++ include/hal.hpp
@@ -95,34 +95,36 @@
  *
  */
 #ifdef ESP32_N16R4
+  #define I2C_SDA_PIN   GPIO_NUM_38
+  #define I2C_SCL_PIN   GPIO_NUM_39
 
-  uint8_t GPS_TX = 25;
-  uint8_t GPS_RX = 26;
+  uint8_t GPS_TX = GPIO_NUM_25;
+  uint8_t GPS_RX = GPIO_NUM_26;
 
-  extern const uint8_t BOARD_BOOT_PIN = 0;
+  extern const uint8_t BOARD_BOOT_PIN = GPIO_NUM_0;
 
-  extern const uint8_t TFT_SPI_SCLK = 14;
-  extern const uint8_t TFT_SPI_MOSI = 13;
-  extern const uint8_t TFT_SPI_MISO = 27;
-  extern const uint8_t TFT_SPI_DC   = 15;
-  extern const uint8_t TFT_SPI_CS   = 2;
-  extern const uint8_t TFT_SPI_RST  = 32;
+  extern const uint8_t TFT_SPI_SCLK = GPIO_NUM_14;
+  extern const uint8_t TFT_SPI_MOSI = GPIO_NUM_13;
+  extern const uint8_t TFT_SPI_MISO = GPIO_NUM_27;
+  extern const uint8_t TFT_SPI_DC   = GPIO_NUM_15;
+  extern const uint8_t TFT_SPI_CS   = GPIO_NUM_2;
+  extern const uint8_t TFT_SPI_RST  = GPIO_NUM_32;
 
-  extern const uint8_t TCH_SPI_SCLK = 14;
-  extern const uint8_t TCH_SPI_MOSI = 13;
-  extern const uint8_t TCH_SPI_MISO = 27;
-  extern const uint8_t TCH_SPI_INT  = 5;
-  extern const uint8_t TCH_SPI_CS   = 18;
+  extern const uint8_t TCH_SPI_SCLK = GPIO_NUM_14;
+  extern const uint8_t TCH_SPI_MOSI = GPIO_NUM_13;
+  extern const uint8_t TCH_SPI_MISO = GPIO_NUM_27;
+  extern const uint8_t TCH_SPI_INT  = GPIO_NUM_5;
+  extern const uint8_t TCH_SPI_CS   = GPIO_NUM_18;
 
   extern const uint8_t TCH_I2C_PORT = 0;  
-  extern const uint8_t TCH_I2C_SDA  = 38;
-  extern const uint8_t TCH_I2C_SCL  = 39;
-  extern const uint8_t TCH_I2C_INT  = 40;
+  extern const uint8_t TCH_I2C_SDA  = GPIO_NUM_38;
+  extern const uint8_t TCH_I2C_SCL  = GPIO_NUM_39;
+  extern const uint8_t TCH_I2C_INT  = GPIO_NUM_40;
 
-  extern const uint8_t SD_CS = 4;
-  extern const uint8_t SD_MISO = 19;
-  extern const uint8_t SD_MOSI = 23;
-  extern const uint8_t SD_CLK = 12;
+  extern const uint8_t SD_CS = GPIO_NUM_4;
+  extern const uint8_t SD_MISO = GPIO_NUM_19;
+  extern const uint8_t SD_MOSI = GPIO_NUM_23;
+  extern const uint8_t SD_CLK = GPIO_NUM_12;
 #endif
 
 /**
@@ -130,36 +132,36 @@
  *
  */
 #ifdef ESP32S3_N16R8
-  #define I2C_SDA_PIN 38
-  #define I2C_SCL_PIN 39
+  #define I2C_SDA_PIN   GPIO_NUM_38
+  #define I2C_SCL_PIN   GPIO_NUM_39
 
-  uint8_t GPS_TX = 17;
-  uint8_t GPS_RX = 18;
+  uint8_t GPS_TX = GPIO_NUM_17;
+  uint8_t GPS_RX = GPIO_NUM_18;
 
-  extern const uint8_t BOARD_BOOT_PIN = 0;
+  extern const uint8_t BOARD_BOOT_PIN = GPIO_NUM_0;
 
-  extern const uint8_t TFT_SPI_SCLK = 12;
-  extern const uint8_t TFT_SPI_MOSI = 11;
-  extern const uint8_t TFT_SPI_MISO = 13;
-  extern const uint8_t TFT_SPI_DC   = 3;
-  extern const uint8_t TFT_SPI_CS   = 10;
-  extern const uint8_t TFT_SPI_RST  = 6;
+  extern const uint8_t TFT_SPI_SCLK = GPIO_NUM_12;
+  extern const uint8_t TFT_SPI_MOSI = GPIO_NUM_11;
+  extern const uint8_t TFT_SPI_MISO = GPIO_NUM_13;
+  extern const uint8_t TFT_SPI_DC   = GPIO_NUM_3;
+  extern const uint8_t TFT_SPI_CS   = GPIO_NUM_10;
+  extern const uint8_t TFT_SPI_RST  = GPIO_NUM_6;
 
-  extern const uint8_t TCH_SPI_SCLK = 12;
-  extern const uint8_t TCH_SPI_MOSI = 11;
-  extern const uint8_t TCH_SPI_MISO = 13;
-  extern const uint8_t TCH_SPI_INT  = 5;
-  extern const uint8_t TCH_SPI_CS   = 4;
+  extern const uint8_t TCH_SPI_SCLK = GPIO_NUM_12;
+  extern const uint8_t TCH_SPI_MOSI = GPIO_NUM_11;
+  extern const uint8_t TCH_SPI_MISO = GPIO_NUM_13;
+  extern const uint8_t TCH_SPI_INT  = GPIO_NUM_5;
+  extern const uint8_t TCH_SPI_CS   = GPIO_NUM_4;
 
   extern const uint8_t TCH_I2C_PORT = 0;  
-  extern const uint8_t TCH_I2C_SDA  = 38;
-  extern const uint8_t TCH_I2C_SCL  = 39;
-  extern const uint8_t TCH_I2C_INT  = 40;
+  extern const uint8_t TCH_I2C_SDA  = GPIO_NUM_38;
+  extern const uint8_t TCH_I2C_SCL  = GPIO_NUM_39;
+  extern const uint8_t TCH_I2C_INT  = GPIO_NUM_40;
 
-  extern const uint8_t SD_CS = 1;
-  extern const uint8_t SD_MISO = 41;
-  extern const uint8_t SD_MOSI = 2;
-  extern const uint8_t SD_CLK = 42;
+  extern const uint8_t SD_CS = GPIO_NUM_1;
+  extern const uint8_t SD_MISO = GPIO_NUM_41;
+  extern const uint8_t SD_MOSI = GPIO_NUM_2;
+  extern const uint8_t SD_CLK = GPIO_NUM_42;
 #endif
 
 /**
