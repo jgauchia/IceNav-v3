a46f4e650b4b2ef60d2da0f13e5f850d32075c4d
jgauchia <jgauchia@gmx.es>
2025-09-19 20:03:18 +0200

    fix(build): Fix MAKERFABS build

--- lib/panel/panelSelect.hpp
+++ lib/panel/panelSelect.hpp
@@ -30,6 +30,8 @@
     #include "ICENAV_BOARD.hpp"
 #elif defined(TDECK_ESP32S3)
     #include "LILYGO_TDECK.hpp"
+#elif defined(MAKERF_ESP32S3)
+    #include "MAKERF_ESP32S3.hpp"
 #else
     #error "No Panel defined!"
 #endif
