97ea934b98f70c591d606b5b045be7dcd243321c
jgauchia <jgauchia@gmx.es>
2025-12-28 21:02:09 +0100

    refact(bme): Replace Adafruit BME280 with native ESP-IDF driver

--- lib/bme/bme.hpp
+++ lib/bme/bme.hpp
@@ -1,7 +1,7 @@
 /**
  * @file bme.hpp
  * @author Jordi Gauchía (jgauchia@jgauchia.com)
- * @brief  BME280 Sensor functions
+ * @brief  BME280 Sensor functions - Native ESP-IDF driver
  * @version 0.2.4
  * @date 2025-12
  */
@@ -12,13 +12,57 @@
 
 #define ENABLE_TEMP
 
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include <Wire.h>
+#include <cstdint>
 
-#define BME_ADDRESS 0x76          /**< I2C address for the BME280 sensor (default: 0x76). */
-extern Adafruit_BME280 bme;       /**< Global instance of the BME280 sensor driver. */
-extern uint8_t tempValue;         /**< Stores the latest temperature reading from the BME280 sensor. */
-extern uint8_t tempOld;           /**< Stores the previous temperature value to detect changes. */
+#define BME_ADDRESS 0x76
+
+class BME280_Driver
+{
+    private:
+        uint8_t i2cAddr;
+
+        uint16_t dig_T1;
+        int16_t dig_T2;
+        int16_t dig_T3;
+
+        uint16_t dig_P1;
+        int16_t dig_P2;
+        int16_t dig_P3;
+        int16_t dig_P4;
+        int16_t dig_P5;
+        int16_t dig_P6;
+        int16_t dig_P7;
+        int16_t dig_P8;
+        int16_t dig_P9;
+
+        uint8_t dig_H1;
+        int16_t dig_H2;
+        uint8_t dig_H3;
+        int16_t dig_H4;
+        int16_t dig_H5;
+        int8_t dig_H6;
+
+        int32_t t_fine;
+
+        uint8_t read8(uint8_t reg);
+        uint16_t read16_LE(uint8_t reg);
+        int16_t readS16_LE(uint8_t reg);
+        void write8(uint8_t reg, uint8_t value);
+        void readCoefficients();
+
+    public:
+        BME280_Driver();
+        bool begin(uint8_t addr = BME_ADDRESS);
+        float readTemperature();
+        float readPressure();
+        float readHumidity();
+        float readAltitude(float seaLevelPressure = 101325.0f);
+};
+
+extern BME280_Driver bme;
+extern uint8_t tempValue;
+extern uint8_t tempOld;
 
 void initBME();
 
